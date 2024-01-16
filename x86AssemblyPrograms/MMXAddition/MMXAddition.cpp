d25ac6f335387d8f89f27a41637e16baed614cdc
Etjen Buljubašić <buljubasicetjen4@gmail.com>
2024-01-16 21:39:17 +0100

    mmx addition assembly

--- x86AssemblyPrograms/MMXAddition/MMXAddition.cpp
+++ x86AssemblyPrograms/MMXAddition/MMXAddition.cpp
@@ -2,8 +2,21 @@
 //
 
 #include "pch.h"
+#include "MmxVal.h"
 #include <iostream>
 #include <stdlib.h>
+
+enum MmxAddOp : unsigned int {
+	paddb,				//packed byte addition with wraparound
+	paddsb,				//packed byte addition with signed saturation
+	paddusb,			//packed byte addition with unsigned saturation
+	paddw,				//packed word addition with wraparound
+	paddsw,				//packed word addition with signed saturation
+	paddusw,			//packed word addition with unsigned saturation
+	paddd				//packed doubleword addition with wraparound
+};
+
+extern "C" MmxVal MmxAdd(MmxVal a, MmxVal b, MmxAddOp op);
 
 int main()
 {
---
+++ x86AssemblyPrograms/MMXAddition/Mmx_Addition.asm
@@ -0,0 +1,63 @@
+.model flat,c
+
+.code
+MmxAdd proc
+	push ebp
+	mov ebp,esp
+
+	mov eax,[ebp+24]
+	cmp eax,AddOpTableCount
+	jae BadAddOp
+
+	movq mm0,[ebp+8]
+	movq mm1,[ebp+16]
+	jmp [AddOpTable+eax*4]
+
+MmxPaddb:
+	paddb mm0,mm1
+	jmp SaveResult
+
+MmxPaddsb:
+	paddsb mm0,mm1
+	jmp SaveResult
+
+MmxPaddusb:
+	paddusb mm0,mm1
+	jmp SaveResult
+
+MmxPaddw:
+	paddw mm0,mm1
+	jmp SaveResult
+
+MmxPaddsw:
+	paddsw mm0,mm1
+	jmp SaveResult
+
+MmxPaddusw:
+	paddusw mm0,mm1
+	jmp SaveResult
+
+MmxPaddd:
+	paddd mm0,mm1
+	jmp SaveResult
+
+BadAddOp:
+	pxor mm0,mm0
+
+SaveResult:
+	movd eax,mm0
+	pshufw mm2,mm0,01001110b
+	movd edx,mm2
+	emms
+	pop ebp
+
+	ret
+
+	align 4
+AddOpTable:
+	dword MmxPaddb,MmxPaddsb,MmxPaddusb,MmxPaddw,MmxPaddsw,MmxPaddusw,MmxPaddd
+
+AddOpTableCount equ ($-AddOpTable) / size dword
+
+MmxAdd endp
+end
