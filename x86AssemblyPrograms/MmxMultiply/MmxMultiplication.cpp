f2e8ecb7df62bd870e81664771d958275469432a
Etjen Buljubašić <buljubasicetjen4@gmail.com>
2024-01-17 21:28:59 +0100

    mmx multiplication done

--- x86AssemblyPrograms/MmxMultiply/MmxMultiplication.cpp
+++ x86AssemblyPrograms/MmxMultiply/MmxMultiplication.cpp
@@ -4,9 +4,34 @@
 #include "pch.h"
 #include <iostream>
 #include <stdlib.h>
+#include "MmxVal.h"
+
+extern "C" void MmxMultiply(MmxVal a, MmxVal b, MmxVal * prod_lo, MmxVal * prod_hi);
 
 int main()
 {
+	MmxVal a, b;
+	MmxVal prod_lo, prod_hi;
+	char buff[256];
+
+	a.i16[0] = 10;
+	a.i16[1] = 30;
+	a.i16[2] = -50;
+	a.i16[3] = -70;
+
+	b.i16[0] = 2000;
+	b.i16[1] = -4000;
+	b.i16[2] = 6000;
+	b.i16[3] = -8000;
+		
+	MmxMultiply(a, b, &prod_lo, &prod_hi);
+
+	printf("\nResult of MmxMulSignedWord\n");
+	printf("a:%s\n", a.ToString_i16(buff, sizeof(buff)));
+	printf("b:%s\n", b.ToString_i16(buff, sizeof(buff)));
+
+	printf("prod_lo:%s\n", prod_lo.ToString_i32(buff, sizeof(buff)));
+	printf("prod_hi:%s\n", prod_hi.ToString_i32(buff, sizeof(buff)));
 
 	return 0;
 }
