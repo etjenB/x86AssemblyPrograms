ce785262e564eb71096d7286f281704228c0c1d0
Etjen Buljubašić <buljubasicetjen4@gmail.com>
2024-01-17 21:56:22 +0100

    sse floating point arithmetic project created

---
+++ x86AssemblyPrograms/SSEfpArithmetic/SSEfpArithmetic.cpp
@@ -0,0 +1,15 @@
+// SSEfpArithmetic.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <stdlib.h>
+
+extern "C" void SseArithmeticOperationsFloat(float a, float b, float c[8]);
+extern "C" void SseArithmeticOperationsDouble(double a, double b, double c[8]);
+
+int main()
+{
+
+	return 0;
+}
