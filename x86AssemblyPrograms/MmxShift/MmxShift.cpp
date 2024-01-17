b523c01127e6a7342ce4736846118224e39fcf24
Etjen Buljubašić <buljubasicetjen4@gmail.com>
2024-01-17 20:23:17 +0100

    mmx shift project

---
+++ x86AssemblyPrograms/MmxShift/MmxShift.cpp
@@ -0,0 +1,12 @@
+// MmxShift.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <stdlib.h>
+
+int main()
+{
+
+	return 0;
+}
