45bbd0b4dccddcfdf5793388d93cbef4cf73d6f2
Etjen Buljubašić <buljubasicetjen4@gmail.com>
2024-01-17 20:06:48 +0100

    mmx addition cpp examples

--- x86AssemblyPrograms/MMXAddition/MmxVal.cpp
+++ x86AssemblyPrograms/MMXAddition/MmxVal.cpp
@@ -1,3 +1,4 @@
+#include "pch.h"
 #include "MmxVal.h"
 #include <stdio.h>
 
@@ -19,19 +20,19 @@ char* MmxVal::ToString_i64(char* s, size_t len) {
 }
 
 char* MmxVal::ToString_ui8(char* s, size_t len) {
-	sprintf_s(s, len, "%4d %4d %4d %4d %4d %4d %4d %4d", ui8[0], ui8[1], ui8[2], ui8[3], ui8[4], ui8[5], ui8[6], ui8[7]);
+	sprintf_s(s, len, "%4u %4u %4u %4u %4u %4u %4u %4u", ui8[0], ui8[1], ui8[2], ui8[3], ui8[4], ui8[5], ui8[6], ui8[7]);
 	return s;
 }
 char* MmxVal::ToString_ui16(char* s, size_t len) {
-	sprintf_s(s, len, "%8d %8d %8d %8d", ui16[0], ui16[1], ui16[2], ui16[3]);
+	sprintf_s(s, len, "%8u %8u %8u %8u", ui16[0], ui16[1], ui16[2], ui16[3]);
 	return s;
 }
 char* MmxVal::ToString_ui32(char* s, size_t len) {
-	sprintf_s(s, len, "%12d %12d", ui32[0], ui32[1]);
+	sprintf_s(s, len, "%12u %12u", ui32[0], ui32[1]);
 	return s;
 }
 char* MmxVal::ToString_ui64(char* s, size_t len) {
-	sprintf_s(s, len, "%16lld", ui64);
+	sprintf_s(s, len, "%16llu", ui64);
 	return s;
 }
 
