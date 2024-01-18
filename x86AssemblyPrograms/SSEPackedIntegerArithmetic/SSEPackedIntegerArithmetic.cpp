5aae4b36457687a6143ea9fb68d5117856061aff
Etjen Buljubašić <buljubasicetjen4@gmail.com>
2024-01-18 22:44:34 +0100

    sse packed integer arithmetic project created

---
+++ x86AssemblyPrograms/SSEPackedIntegerArithmetic/SSEPackedIntegerArithmetic.cpp
@@ -0,0 +1,12 @@
+// SSEPackedIntegerArithmetic.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <stdlib.h>
+
+int main()
+{
+
+	return 0;
+}
---
+++ x86AssemblyPrograms/SSEPackedIntegerArithmetic/Xmm.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "MiscDef.h"
+
+union MmxVal
+{
+	Int8 i8[16];
+	Int16 i16[8];
+	Int32 i32[4];
+	Int64 i64[2];
+
+	UInt8 ui8[16];
+	UInt16 ui16[8];
+	UInt32 ui32[4];
+	UInt64 ui64[2];
+
+	float r32[4];
+	double r64[2];
+
+	char* ToString_i8(char* s, size_t len);
+	char* ToString_i16(char* s, size_t len);
+	char* ToString_i32(char* s, size_t len);
+	char* ToString_i64(char* s, size_t len);
+
+	char* ToString_ui8(char* s, size_t len);
+	char* ToString_ui16(char* s, size_t len);
+	char* ToString_ui32(char* s, size_t len);
+	char* ToString_ui64(char* s, size_t len);
+
+	char* ToString_x8(char* s, size_t len);
+	char* ToString_x16(char* s, size_t len);
+	char* ToString_x32(char* s, size_t len);
+	char* ToString_x64(char* s, size_t len);
+
+	char* ToString_r32(char* s, size_t len);
+	char* ToString_r64(char* s, size_t len);
+};
